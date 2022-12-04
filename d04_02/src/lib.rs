74e5bc79695a7be3f12c94484ff2904b3765184b
staslock <staslock@gmail.com>
2022-12-04 15:26:03 +0100

    d4 more idiomatic way to get iterator over values

--- d04_02/src/lib.rs
+++ d04_02/src/lib.rs
@@ -1,22 +1,14 @@
 use std::collections::HashSet;
 
-
 fn solve_problem(input_data: &str) -> i32 {
-    let mut lines = input_data.lines();
-    let mut result: i32 = 0;
-
-    for l in lines {
-        let epair = l.split(',').collect::<Vec<&str>>();
-        let e1 = epair[0].split("-").map(|v| v.parse().unwrap()).collect::<Vec<i32>>();
-        let e2 = epair[1].split("-").map(|v| v.parse().unwrap()).collect::<Vec<i32>>();
-
-        if e1[0] <= e2[0] && e2[0] <= e1[1] {
-            result += 1
-        } else if e2[0] <= e1[0] && e1[0] <= e2[1] {
-            result += 1
-        }
-    }
-    result
+    input_data.lines().map(|l| {
+        let w: Vec<i32> = l.split(&[',', '-']).map(|v| v.parse().unwrap()).collect();
+        if w[0] <= w[2] && w[2] <= w[1] {
+            1
+        } else if w[2] <= w[0] && w[0] <= w[3] {
+            1
+        } else { 0 }
+    }).sum()
 }
 
 pub fn solve() -> i32 {
